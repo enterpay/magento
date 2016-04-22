e80f4bac8fc9bbb54052bfa4213e3dfc73de7b2c
Tony Träskelin <tony.traskelin@enterpay.fi>
2016-04-22 15:48:39 +0300

    Small fix when persistent shopping cart in use
    Small fix when persistent shopping cart in use

--- app/code/community/Enterpay/LaskuYritykselle/Model/Observer.php
+++ app/code/community/Enterpay/LaskuYritykselle/Model/Observer.php
@@ -6,7 +6,7 @@ class Enterpay_LaskuYritykselle_Model_Observer extends Mage_Core_Model_Abstract
     public function sales_model_service_quote_submit_after(Varien_Event_Observer $observer) {
 
         $payment = $observer->getQuote()->getPayment();
-        if (strtolower($payment['method']) == 'laskuyritykselle') {
+        if (strtolower($payment['method']) == 'laskuyritykselle' && Mage::getStoreConfig('persistent/options/enabled') != 1) {
             // Activate the quote
             $observer->getQuote()->setIsActive(true);
         }
